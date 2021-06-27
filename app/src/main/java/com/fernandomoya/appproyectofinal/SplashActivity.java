1b102f32e52636d4b452fec95a4b320906829de4
Fernando Moya <fernandoalexander2014@gmail.com>
2021-06-26 22:01:24 -0500

    Changes in splash screen

--- app/src/main/java/com/fernandomoya/appproyectofinal/SplashActivity.java
+++ app/src/main/java/com/fernandomoya/appproyectofinal/SplashActivity.java
@@ -4,14 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 2500;
+
+    ImageView imageView;
+    TextView txtNombre;
+    Animation top, bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, ChoiceActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash);
+        imageView = findViewById(R.id.imageView);
+        txtNombre = findViewById(R.id.txtNombre);
+
+
+        top = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom);
+        imageView.setAnimation(top);
+        txtNombre.setAnimation(bottom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
+
     }
+
+
 }
