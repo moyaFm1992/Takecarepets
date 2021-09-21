ea0630c234e87e72e1344f381f94d1c3292e2d46
Fernando Moya <fernandoalexander2014@gmail.com>
2021-09-21 08:36:41 -0500

    Changing Boolean

--- app/src/main/java/com/fernandomoya/appproyectofinal/WelcomeActivity.java
+++ app/src/main/java/com/fernandomoya/appproyectofinal/WelcomeActivity.java
@@ -124,7 +124,7 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     private void launchHomeScreen() {
-        prefManager.setFirstTimeLaunch(false);
+        prefManager.setFirstTimeLaunch(Boolean.FALSE);
         startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
         finish();
     }
--- app/src/main/java/com/fernandomoya/appproyectofinal/model/PrefManager.java
+++ app/src/main/java/com/fernandomoya/appproyectofinal/model/PrefManager.java
@@ -29,7 +29,7 @@ public class PrefManager {
     }
 
     public boolean isFirstTimeLaunch() {
-        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, Boolean.TRUE);
     }
 
 }
